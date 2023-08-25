bf48549ac2947ffbec55a49d86149248fe3513bd
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-08-25 09:11:50 -0700

    Create Variables_and_Data_Types.py

---
+++ Variables_and_Data_Types.py
@@ -0,0 +1,25 @@
+
+food = "ice cream"
+money = 2000
+
+age = 30
+age = age + 1
+print(age)
+
+# String - Used for combinations of characters, such as letters or symbols
+# Integer - Whole numbers
+# Float - Numbers that contain decimal points or for fractions
+# Boolean - Used for data that is restricted to True or False options
+# List - Series of different data types stored in a collection
+
+# String        | Float     | Integer       | Boolean   | List
+# Title         | Rating    | Times Viewed  | Favorite  | Seen By
+# Star Wars     | 9.8       | 13            | True      | Alice, Bob
+# Matrix        | 8.5       | 23            | False     | Charlie
+# Indiana Jones | 6.1       | 3             | False     | Deniel, Evie
+
+height = 200
+print (height)
+
+height = height + 50
+print (height)
