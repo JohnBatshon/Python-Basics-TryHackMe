096cf32a8582930ce99358d2a1026f6669a14eaa
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-08-25 09:00:14 -0700

    Create Mathmatical_Operators.py

---
+++ Mathmatical_Operators.py
@@ -0,0 +1,25 @@
+# Operator          | Syntax    | Example
+# Addition          | +         | 1 + 1 = 2
+# Subtraction       | -         | 5 - 1 = 4
+# Multiplication    | *         | 10 * 10 = 100
+# Division          | /         | 10 / 2 = 5
+# Modulus           | %         | 10 % 2 = 0
+# Exponent          | **        | 5**2 = 25(5 to the 2nd Power)
+
+# Comparison Operators (Typically used in loops & if statemetns)
+
+# Symbol                    | Syntax
+# Greater Than              | >
+# Less Than                 | <
+# Equal to                  | ==
+# Not Equal to              | !=
+# Greater than or equal to  | >=
+# Less than or equal to     | <=
+
+print(21 + 43)
+
+print(142-52)
+
+print(10*342)
+
+print(5**2)
